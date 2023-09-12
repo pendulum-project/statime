b2e38632f7f54b360189039edecfed79d0817493
Tamme Dittrich <tamme@tweedegolf.com>
2023-09-12 15:12:31 +0200

    Tx timestamping working

--- statime-stm32/src/ethernet.rs
+++ statime-stm32/src/ethernet.rs
@@ -1,13 +1,17 @@
-use smoltcp::{iface::SocketStorage, socket::udp};
-use stm32_eth::dma::{RxRingEntry, TxRingEntry};
+use rtic_monotonics::{systick::Systick, Monotonic};
+use smoltcp::{
+    iface::{Interface, SocketSet, SocketStorage},
+    socket::udp,
+};
+use stm32_eth::dma::{EthernetDMA, RxRingEntry, TxRingEntry};
 
 pub struct NetworkResources {
     pub rx_ring: [RxRingEntry; 2],
     pub tx_ring: [TxRingEntry; 2],
     pub rx_meta_storage: [udp::PacketMetadata; 8],
-    pub rx_payload_storage: [u8; 1024],
+    pub rx_payload_storage: [u8; 8192],
     pub tx_meta_storage: [udp::PacketMetadata; 8],
-    pub tx_payload_storage: [u8; 1024],
+    pub tx_payload_storage: [u8; 8192],
     pub sockets: [SocketStorage<'static>; 8],
 }
 
@@ -17,10 +21,35 @@ impl NetworkResources {
             rx_ring: [RxRingEntry::new(), RxRingEntry::new()],
             tx_ring: [TxRingEntry::new(), TxRingEntry::new()],
             rx_meta_storage: [udp::PacketMetadata::EMPTY; 8],
-            rx_payload_storage: [0; 1024],
+            rx_payload_storage: [0; 8192],
             tx_meta_storage: [udp::PacketMetadata::EMPTY; 8],
-            tx_payload_storage: [0; 1024],
+            tx_payload_storage: [0; 8192],
             sockets: [SocketStorage::EMPTY; 8],
         }
     }
+}
+
+pub struct NetworkStack {
+    pub dma: EthernetDMA<'static, 'static>,
+    pub iface: Interface,
+    pub sockets: SocketSet<'static>,
+}
+
+impl NetworkStack {
+    pub fn poll(&mut self) {
+        self.iface
+            .poll(now(), &mut &mut self.dma, &mut self.sockets);
+    }
+
+    pub fn poll_delay(&mut self) -> Option<smoltcp::time::Duration> {
+        self.iface.poll_delay(now(), &self.sockets)
+    }
+}
+
+pub const CLIENT_ADDR: [u8; 6] = [0x80, 0x00, 0xde, 0xad, 0xbe, 0xef];
+
+fn now() -> smoltcp::time::Instant {
+    let now_millis = Systick::now().ticks();
+    // TODO handle case where systick is not 1kHz
+    smoltcp::time::Instant::from_millis(now_millis)
 }
--- statime-stm32/src/main.rs
+++ statime-stm32/src/main.rs
@@ -13,60 +13,39 @@ pub mod ptp_clock;
 mod app {
 
     use defmt::unwrap;
+    use futures::{future::FutureExt, select_biased};
     use ieee802_3_miim::{
         phy::{PhySpeed, LAN8742A},
         Phy,
     };
-    use rtic_monotonics::{
-        systick::{fugit::RateExtU32, ExtU32, Systick},
-        Monotonic,
+    use rtic_monotonics::systick::{fugit::RateExtU32, ExtU32, Systick};
+    use rtic_sync::{
+        channel::{Receiver, Sender},
+        make_channel,
     };
     use smoltcp::{
         iface::{Config, Interface, SocketHandle, SocketSet},
         socket::udp::{self},
         wire::{EthernetAddress, IpAddress, IpCidr, Ipv4Address},
     };
-    use stm32_eth::{dma::EthernetDMA, mac, EthPins, Parts, PartsIn};
+    use stm32_eth::{mac, EthPins, Parts, PartsIn};
     use stm32f7xx_hal::{
         gpio::{Output, Pin, Speed},
         prelude::*,
     };
 
-    use crate::ethernet::NetworkResources;
-
-    pub struct NetworkStack {
-        dma: EthernetDMA<'static, 'static>,
-        iface: Interface,
-        sockets: SocketSet<'static>,
-    }
-
-    impl NetworkStack {
-        pub fn poll(&mut self) {
-            self.iface
-                .poll(now(), &mut &mut self.dma, &mut self.sockets);
-        }
-
-        pub fn poll_delay(&mut self) -> Option<smoltcp::time::Duration> {
-            self.iface.poll_delay(now(), &self.sockets)
-        }
-    }
-
-    const CLIENT_ADDR: [u8; 6] = [0x80, 0x00, 0xde, 0xad, 0xbe, 0xef];
-
-    fn now() -> smoltcp::time::Instant {
-        let now_micros = Systick::now().ticks() * 1000;
-        smoltcp::time::Instant::from_micros(now_micros as i64)
-    }
+    use crate::ethernet::{NetworkResources, NetworkStack, CLIENT_ADDR};
 
     #[shared]
     struct Shared {
         net: NetworkStack,
-        socket: SocketHandle,
     }
 
     #[local]
     struct Local {
         led_pin: Pin<'B', 7, Output>,
+        please_poll_s: Sender<'static, (), 1>,
+        tx_done_s: Sender<'static, (), 1>,
     }
 
     #[init(local = [net_resources: NetworkResources = NetworkResources::new()])]
@@ -168,6 +147,7 @@ mod app {
         defmt::info!("Enabling interrupts");
         dma.enable_interrupt();
 
+        // Setup PHY
         let mut phy = LAN8742A::new(mac, 0);
 
         phy.phy_init();
@@ -193,8 +173,11 @@ mod app {
         // Setup PPS
         ptp.enable_pps(pps);
         ptp.set_pps_freq(0);
-
         // todo handle addend
+
+        type Empty = ();
+        let (please_poll_s, please_poll_r) = make_channel!(Empty, 1);
+        let (tx_done_s, tx_done_r) = make_channel!(Empty, 1);
 
         let net = NetworkStack {
             dma,
@@ -203,15 +186,16 @@ mod app {
         };
 
         unwrap!(blinky::spawn());
-        unwrap!(udp_ping::spawn());
-        unwrap!(poll_smoltcp::spawn());
+        udp_ping::spawn(udp_socket, please_poll_s.clone(), tx_done_r).unwrap();
+        poll_smoltcp::spawn(please_poll_r).unwrap();
 
         (
-            Shared {
-                net,
-                socket: udp_socket,
+            Shared { net },
+            Local {
+                led_pin,
+                please_poll_s,
+                tx_done_s,
             },
-            Local { led_pin },
         )
     }
 
@@ -226,60 +210,99 @@ mod app {
         }
     }
 
-    #[task(shared = [socket, net], priority = 1)]
-    async fn udp_ping(cx: udp_ping::Context) {
-        let mut net_sock = (cx.shared.net, cx.shared.socket);
+    #[task(shared = [net], priority = 1)]
+    async fn udp_ping(
+        mut cx: udp_ping::Context,
+        socket: SocketHandle,
+        mut please_poll: Sender<'static, (), 1>,
+        mut tx_done_r: Receiver<'static, (), 1>,
+    ) {
+        let mut meta: udp::UdpMetadata = smoltcp::wire::IpEndpoint {
+            addr: IpAddress::Ipv4(Ipv4Address([10, 0, 0, 1])),
+            port: 1337,
+        }
+        .into();
 
         loop {
-            Systick::delay(10000.millis()).await;
+            Systick::delay(1000.millis()).await;
 
-            let mut meta: udp::UdpMetadata = smoltcp::wire::IpEndpoint {
-                addr: IpAddress::Ipv4(Ipv4Address([10, 0, 0, 1])),
-                port: 1337,
-            }
-            .into();
-
-            net_sock.lock(|net, s| {
+            let packet_id = cx.shared.net.lock(|net| {
                 let packet_id = net.dma.next_packet_id();
-                meta.meta = packet_id.into();
+                meta.meta = packet_id.clone().into();
 
                 defmt::println!("to: {}", meta);
 
                 let result = net
                     .sockets
-                    .get_mut::<udp::Socket>(*s)
+                    .get_mut::<udp::Socket>(socket)
                     .send_slice(&[0x42; 42], meta);
 
                 match result {
-                    Ok(_) => (),
+                    Ok(()) => (),
                     Err(e) => defmt::error!("Could not sent UDP packet because: {}", e),
                 }
 
+                // Send out the packet, this makes sure the MAC actually dets the packet and
+                // knows about the packet_id
                 net.poll();
+
+                packet_id
             });
+
             defmt::trace!("sent udp");
+
+            let timestamp = loop {
+                let poll = cx
+                    .shared
+                    .net
+                    .lock(|net| net.dma.poll_tx_timestamp(&packet_id));
+
+                let _ = match poll {
+                    core::task::Poll::Ready(result) => break result,
+                    core::task::Poll::Pending => tx_done_r.recv().await,
+                };
+            };
+
+            defmt::info!("Timestamp for {}: {}", packet_id, timestamp);
         }
     }
 
     #[task(shared = [net], priority = 1)]
-    async fn poll_smoltcp(mut cx: poll_smoltcp::Context) {
+    async fn poll_smoltcp(
+        mut cx: poll_smoltcp::Context,
+        mut please_poll: Receiver<'static, (), 1>,
+    ) {
         loop {
-            defmt::trace!("poll");
+            // Let smoltcp handle its things
             let delay_millis = cx.shared.net.lock(|net| {
                 net.poll();
-                net.poll_delay().map(|d| d.total_millis()).unwrap_or(100)
+                net.poll_delay().map(|d| d.total_millis())
             });
 
-            Systick::delay(u32::try_from(delay_millis).unwrap_or(100).millis()).await;
+            // And wait until it wants to get polled again, we want to send something, or we
+            // received something
+            if let Some(delay_millis) = delay_millis {
+                let delay = u32::try_from(delay_millis).unwrap_or(1_000_000).millis();
+                select_biased! {
+                    _ = Systick::delay(delay).fuse() => (),
+                    _ = please_poll.recv().fuse() => (),
+                };
+            } else {
+                let _ = please_poll.recv().await;
+            }
         }
     }
 
-    #[task(binds = ETH, shared = [net], priority = 2)]
-    fn eth_interrupt(mut cx: eth_interrupt::Context) {
-        stm32_eth::eth_interrupt_handler();
+    #[task(binds = ETH, local = [please_poll_s, tx_done_s], priority = 2)]
+    fn eth_interrupt(cx: eth_interrupt::Context) {
+        let reason = stm32_eth::eth_interrupt_handler();
 
-        cx.shared.net.lock(|net| {
-            net.poll();
-        })
+        if reason.rx {
+            let _ = cx.local.please_poll_s.try_send(());
+        }
+
+        if reason.tx {
+            let _ = cx.local.tx_done_s.try_send(());
+        }
     }
 }
