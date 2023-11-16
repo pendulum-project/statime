7dd659161f9e2527c445f8e8319cb4dc25c1c640
Ruben Nijveld <ruben@tweedegolf.com>
2023-11-16 11:28:39 +0100

    Setup documentation builder

---
+++ utils/generate-man.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+
+set -eo pipefail
+
+docs_dir="docs/man"
+output_dir="${1:-"docs/precompiled/man"}"
+files=("statime-linux.8")
+
+mkdir -p "$output_dir"
+
+for f in "${files[@]}"; do
+    origin_file="$docs_dir/$f.md"
+    tmp_file="$output_dir/$f.md"
+    target_file="$output_dir/$f"
+
+    echo "Generating man page for $f from '$origin_file' to '$target_file'"
+    sed '/<!-- ---/s/<!-- ---/---/' "$origin_file" | sed '/--- -->/s/--- -->/---/' > "$tmp_file"
+    utils/pandoc.sh -s -t man "$tmp_file" -o "$target_file"
+    rm "$tmp_file"
+done
