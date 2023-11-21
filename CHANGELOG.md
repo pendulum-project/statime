17cae00ce52da12117f29c2428d333815214a069
Ruben Nijveld <ruben@tweedegolf.com>
2023-11-21 13:56:33 +0100

    Initial setup of build

---
+++ CHANGELOG.md
@@ -0,0 +1,5 @@
+# Changelog
+
+## [unreleased]
+
+[unreleased]: https://github.com/pendulum-project/ntpd-rs/compare/v0.1.0...main
---
+++ pkg/test-scripts/test-statime.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+set -eo pipefail
+set -x
+
+case $1 in
+  post-install|post-upgrade)
+      # Ensure users are created
+      id statime
+
+      # Ensure deamon is present
+      echo -e "\nSTATIME HELP OUTPUT:"
+      /usr/bin/statime --help
+
+      # # Ensure that the systemd service is running
+      # systemctl is-active statime.service --quiet
+    ;;
+esac
