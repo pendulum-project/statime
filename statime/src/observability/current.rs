52915e6f986c0f6c4d46db80419c5235fdc8c831
marlonp <marlonpeeters@tweedegolf.com>
2024-02-29 14:28:44 +0100

    Removed unused dataset fields

--- statime/src/observability/current.rs
+++ statime/src/observability/current.rs
@@ -2,6 +2,9 @@ use crate::datastructures::datasets::InternalCurrentDS;
 
 /// A concrete implementation of the PTP Current dataset (IEEE1588-2019 section
 /// 8.2.2)
+///
+/// Note that the `meanDelay` field from IEEE1588-2019 section 8.2.2.4 is missing
+/// since this field can be constructed from the portDS.
 #[derive(Debug, Default, Copy, Clone)]
 #[cfg_attr(feature = "serde", derive(serde::Serialize, serde::Deserialize))]
 pub struct CurrentDS {
@@ -9,6 +12,8 @@ pub struct CurrentDS {
     pub steps_removed: u16,
     /// See *IEEE1588-2019 section 8.2.2.3*.
     pub offset_from_master: i128,
+    // /// See *IEEE1588-2019 section 8.2.2.4*.
+    // pub mean_delay: i128,
 }
 
 impl From<&InternalCurrentDS> for CurrentDS {
@@ -16,6 +21,7 @@ impl From<&InternalCurrentDS> for CurrentDS {
         Self {
             steps_removed: v.steps_removed,
             offset_from_master: v.offset_from_master.nanos_rounded(),
+            // mean_delay: v.mean_delay.nanos_rounded(),
         }
     }
 }
