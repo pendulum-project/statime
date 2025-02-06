b9b12697a07f73e1e16a90ffc315545339d26b4c
Tamme Dittrich <tamme@tweedegolf.com>
2025-02-06 15:23:16 +0100

    Add metrics exporter service
    This was still missing after adding the metrics exporter binary.

--- pkg/test-scripts/test-statime.sh
+++ pkg/test-scripts/test-statime.sh
@@ -12,7 +12,12 @@ case $1 in
       echo -e "\nSTATIME HELP OUTPUT:"
       /usr/bin/statime --help
 
+      # Ensure deamon is present
+      echo -e "\nSTATIME METRICS EXPORTER HELP OUTPUT:"
+      /usr/bin/statime-metrics-exporter --help
+
       # # Ensure that the systemd service is not running by default
       # ! systemctl is-active statime.service --quiet
+      # ! systemctl is-active statime-metrics-exporter.service --quiet
     ;;
 esac
