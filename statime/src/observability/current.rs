91f613212782208089e1c97b2ef31661184c31d0
David Venhoek <david@tweedegolf.com>
2025-03-12 10:15:31 +0100

    Implemented missing fields in the current dataset.

--- statime/src/observability/current.rs
+++ statime/src/observability/current.rs
@@ -1,4 +1,4 @@
-use crate::datastructures::datasets::InternalCurrentDS;
+use crate::{datastructures::datasets::InternalCurrentDS, filters::FilterEstimate, time::Duration};
 
 /// A concrete implementation of the PTP Current dataset (IEEE1588-2019 section
 /// 8.2.2)
@@ -11,14 +11,27 @@ pub struct CurrentDS {
     /// See *IEEE1588-2019 section 8.2.2.2*.
     pub steps_removed: u16,
     /// See *IEEE1588-2019 section 8.2.2.3*.
-    pub offset_from_master: i128,
+    pub offset_from_master: Duration,
+    /// See *IEEE1588-2019 section 8.2.2.3*.
+    pub mean_delay: Duration,
 }
 
-impl From<&InternalCurrentDS> for CurrentDS {
-    fn from(v: &InternalCurrentDS) -> Self {
-        Self {
-            steps_removed: v.steps_removed,
-            offset_from_master: v.offset_from_master.nanos_rounded(),
+impl CurrentDS {
+    pub(crate) fn from_state(
+        current_ds: &InternalCurrentDS,
+        port_contribution: Option<FilterEstimate>,
+    ) -> Self {
+        match port_contribution {
+            Some(port_contribution) => Self {
+                steps_removed: current_ds.steps_removed,
+                offset_from_master: port_contribution.offset_from_master,
+                mean_delay: port_contribution.mean_delay,
+            },
+            None => Self {
+                steps_removed: current_ds.steps_removed,
+                offset_from_master: Duration::ZERO,
+                mean_delay: Duration::ZERO,
+            },
         }
     }
 }
